aeb0f1d09903cb3befdb67147e8fea7f09f9e2c4
Ming Ming <mint2648@gmail.com>
2014-10-25 17:43:36 +0800

    Add base log strategy for ostream related ends

---
+++ inc/libutils/io/ostream_log_strategy.h
@@ -0,0 +1,47 @@
+/*
+ * ostream_log_strategy.h
+ *
+ * Author: Ming Tsang
+ * Copyright (c) 2014 Ming Tsang
+ * Refer to LICENSE for details
+ */
+
+#pragma once
+
+#include <ostream>
+
+#include "libutils/io/log_strategy.h"
+
+namespace libutils
+{
+namespace io
+{
+
+template<typename CharT_>
+class OstreamLogStrategy : public LogStrategy<CharT_>
+{
+public:
+	void Flush()
+	{
+		GetStream().flush();
+	}
+
+protected:
+	explicit OstreamLogStrategy(std::basic_ostream<CharT_> *stream)
+			: OstreamLogStrategy(stream, true)
+	{}
+	OstreamLogStrategy(std::basic_ostream<CharT_> *stream, const bool is_owner);
+	~OstreamLogStrategy();
+
+	std::basic_ostream<CharT_>& GetStream()
+	{
+		return *m_stream;
+	}
+
+private:
+	std::basic_ostream<CharT_> *m_stream;
+	bool m_is_owner;
+};
+
+}
+}
---
+++ inc/libutils/io/ostream_log_strategy.tcc
@@ -0,0 +1,37 @@
+/*
+ * ostream_log_strategy.tcc
+ *
+ * Author: Ming Tsang
+ * Copyright (c) 2014 Ming Tsang
+ * Refer to LICENSE for details
+ */
+
+#pragma once
+
+#include <ostream>
+
+#include "libutils/io/ostream_log_strategy.h"
+
+namespace libutils
+{
+namespace io
+{
+
+template<typename CharT_>
+OstreamLogStrategy<CharT_>::OstreamLogStrategy(
+		std::basic_ostream<CharT_> *stream, const bool is_owner)
+		: m_stream(stream),
+		  m_is_owner(is_owner)
+{}
+
+template<typename CharT_>
+OstreamLogStrategy<CharT_>::~OstreamLogStrategy()
+{
+	if (m_is_owner)
+	{
+		delete m_stream;
+	}
+}
+
+}
+}
