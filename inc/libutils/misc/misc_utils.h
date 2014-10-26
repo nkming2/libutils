d743fc0e0ce543026dcc05c5d7a620d564570028
Ming Ming <mint2648@gmail.com>
2014-10-27 02:18:22 +0800

    Add method to construct a string in templated CharT

---
+++ inc/libutils/misc/misc_utils.h
@@ -0,0 +1,36 @@
+/*
+ * misc_utils.h
+ *
+ * Author: Ming Tsang
+ * Copyright (c) 2014 Ming Tsang
+ * Refer to LICENSE for details
+ */
+
+#pragma once
+
+#include <string>
+
+namespace libutils
+{
+namespace misc
+{
+
+class MiscUtils
+{
+public:
+	MiscUtils() = delete;
+
+	/**
+	 * Return a basic_string of various char type from a char literal
+	 *
+	 * @param literal
+	 * @return
+	 */
+	template<typename CharT_, size_t kLength>
+	static std::basic_string<CharT_> TemplateStr(const char (&literal)[kLength]);
+};
+
+}
+}
+
+#include "misc_utils.tcc"
---
+++ inc/libutils/misc/misc_utils.tcc
@@ -0,0 +1,25 @@
+/*
+ * misc_utils.tcc
+ *
+ * Author: Ming Tsang
+ * Copyright (c) 2014 Ming Tsang
+ * Refer to LICENSE for details
+ */
+
+#pragma once
+
+#include <string>
+
+namespace libutils
+{
+namespace misc
+{
+
+template<typename CharT_, size_t kLength>
+std::basic_string<CharT_> MiscUtils::TemplateStr(const char (&literal)[kLength])
+{
+	return std::basic_string<CharT_>(literal, literal + kLength - 1);
+}
+
+}
+}
