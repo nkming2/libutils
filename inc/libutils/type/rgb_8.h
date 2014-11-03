1fa7d7529e8531ab9815877dcd35c6ca69c0c318
Ming Ming <mint2648@gmail.com>
2014-11-03 13:02:25 +0800

    Add color structs

---
+++ inc/libutils/type/rgb_8.h
@@ -0,0 +1,137 @@
+/*
+ * rgb_8.h
+ *
+ * Author: Ming Tsang
+ * Copyright (c) 2014 Ming Tsang
+ * Refer to LICENSE for details
+ */
+
+#pragma once
+
+#include <cstdint>
+#include <algorithm>
+
+#include "libutils/math/math_utils.h"
+
+namespace libutils
+{
+namespace type
+{
+
+struct Rgb8
+{
+	Rgb8()
+			: Rgb8(0)
+	{}
+
+	Rgb8(const Rgb8 &rhs) = default;
+
+	explicit Rgb8(const uint8_t val)
+			: Rgb8(val, val, val)
+	{}
+
+	Rgb8(const uint8_t r, const uint8_t g, const uint8_t b)
+			: r(r), g(g), b(b)
+	{}
+
+	Rgb8& operator=(const Rgb8 &rhs)
+	{
+		r = rhs.r;
+		g = rhs.g;
+		b = rhs.b;
+		return *this;
+	}
+
+	friend Rgb8& operator+=(Rgb8 &lhs, const Rgb8 &rhs)
+	{
+		lhs.r = std::min<int>(lhs.r + rhs.r, 0xFF);
+		lhs.g = std::min<int>(lhs.g + rhs.g, 0xFF);
+		lhs.b = std::min<int>(lhs.b + rhs.b, 0xFF);
+		return lhs;
+	}
+
+	friend Rgb8 operator+(const Rgb8 &lhs, const Rgb8 &rhs)
+	{
+		Rgb8 temp(lhs);
+		return temp += rhs;
+	}
+
+	friend Rgb8& operator-=(Rgb8 &lhs, const Rgb8 &rhs)
+	{
+		lhs.r = std::max<int>(lhs.r - rhs.r, 0);
+		lhs.g = std::max<int>(lhs.g - rhs.g, 0);
+		lhs.b = std::max<int>(lhs.b - rhs.b, 0);
+		return lhs;
+	}
+
+	friend Rgb8 operator-(const Rgb8 &lhs, const Rgb8 &rhs)
+	{
+		Rgb8 temp(lhs);
+		return temp -= rhs;
+	}
+
+	friend Rgb8& operator*=(Rgb8 &lhs, const float rhs)
+	{
+		lhs.r = math::MathUtils::Clamp<int>(0, lhs.r * rhs, 0xFF);
+		lhs.g = math::MathUtils::Clamp<int>(0, lhs.g * rhs, 0xFF);
+		lhs.b = math::MathUtils::Clamp<int>(0, lhs.b * rhs, 0xFF);
+		return lhs;
+	}
+
+	friend Rgb8 operator*(const Rgb8 &lhs, const float rhs)
+	{
+		Rgb8 temp(lhs);
+		return temp *= rhs;
+	}
+
+	friend Rgb8& operator/=(Rgb8 &lhs, const float rhs)
+	{
+		lhs.r = math::MathUtils::Clamp<int>(0, lhs.r / rhs, 0xFF);
+		lhs.g = math::MathUtils::Clamp<int>(0, lhs.g / rhs, 0xFF);
+		lhs.b = math::MathUtils::Clamp<int>(0, lhs.b / rhs, 0xFF);
+		return lhs;
+	}
+
+	friend Rgb8 operator/(const Rgb8 &lhs, const float rhs)
+	{
+		Rgb8 temp(lhs);
+		return temp /= rhs;
+	}
+
+	friend Rgb8 operator-(const Rgb8 &lhs)
+	{
+		return Rgb8(0xFF) - lhs;
+	}
+
+	friend bool operator==(const Rgb8 &lhs, const Rgb8 &rhs)
+	{
+		return (lhs.r == rhs.r && lhs.g == rhs.g && lhs.b == rhs.b);
+	}
+
+	friend bool operator!=(const Rgb8 &lhs, const Rgb8 &rhs)
+	{
+		return !(lhs == rhs);
+	}
+
+	uint8_t GetIntensity() const
+	{
+		return std::min<int>(0.2126f * r + 0.7152f * g + 0.0722f * b, 0xFF);
+	}
+
+	uint32_t ToRgb32() const
+	{
+		return 0 | (r << 16) | (g << 8) | b;
+	}
+
+	static Rgb8 FromRgb32(const uint32_t rgb32)
+	{
+		return {static_cast<uint8_t>(rgb32 >> 16),
+			static_cast<uint8_t>(rgb32 >> 8),
+			static_cast<uint8_t>(rgb32)};
+	}
+
+	uint8_t r, g, b;
+};
+
+}
+}
---
+++ inc/libutils/type/rgb_f.h
@@ -0,0 +1,127 @@
+/*
+ * rgb_f.h
+ *
+ * Author: Ming Tsang
+ * Copyright (c) 2014 Ming Tsang
+ * Refer to LICENSE for details
+ */
+
+#pragma once
+
+#include <algorithm>
+
+#include "libutils/math/math_utils.h"
+#include "libutils/type/floating_point.h"
+
+namespace libutils
+{
+namespace type
+{
+
+struct RgbF
+{
+	RgbF()
+			: RgbF(0)
+	{}
+
+	RgbF(const RgbF &rhs) = default;
+
+	explicit RgbF(const float val)
+			: RgbF(val, val, val)
+	{}
+
+	RgbF(const float r, const float g, const float b)
+			: r(r), g(g), b(b)
+	{}
+
+	RgbF& operator=(const RgbF &rhs)
+	{
+		r = rhs.r;
+		g = rhs.g;
+		b = rhs.b;
+		return *this;
+	}
+
+	friend RgbF& operator+=(RgbF &lhs, const RgbF &rhs)
+	{
+		lhs.r = std::min<float>(lhs.r + rhs.r, 1.0f);
+		lhs.g = std::min<float>(lhs.g + rhs.g, 1.0f);
+		lhs.b = std::min<float>(lhs.b + rhs.b, 1.0f);
+		return lhs;
+	}
+
+	friend RgbF operator+(const RgbF &lhs, const RgbF &rhs)
+	{
+		RgbF temp(lhs);
+		return temp += rhs;
+	}
+
+	friend RgbF& operator-=(RgbF &lhs, const RgbF &rhs)
+	{
+		lhs.r = std::max<float>(lhs.r - rhs.r, 0.0f);
+		lhs.g = std::max<float>(lhs.g - rhs.g, 0.0f);
+		lhs.b = std::max<float>(lhs.b - rhs.b, 0.0f);
+		return lhs;
+	}
+
+	friend RgbF operator-(const RgbF &lhs, const RgbF &rhs)
+	{
+		RgbF temp(lhs);
+		return temp -= rhs;
+	}
+
+	friend RgbF& operator*=(RgbF &lhs, const float rhs)
+	{
+		lhs.r = math::MathUtils::Clamp<float>(0.0f, lhs.r * rhs, 1.0f);
+		lhs.g = math::MathUtils::Clamp<float>(0.0f, lhs.g * rhs, 1.0f);
+		lhs.b = math::MathUtils::Clamp<float>(0.0f, lhs.b * rhs, 1.0f);
+		return lhs;
+	}
+
+	friend RgbF operator*(const RgbF &lhs, const float rhs)
+	{
+		RgbF temp(lhs);
+		return temp *= rhs;
+	}
+
+	friend RgbF& operator/=(RgbF &lhs, const float rhs)
+	{
+		lhs.r = math::MathUtils::Clamp<float>(0.0f, lhs.r / rhs, 1.0f);
+		lhs.g = math::MathUtils::Clamp<float>(0.0f, lhs.g / rhs, 1.0f);
+		lhs.b = math::MathUtils::Clamp<float>(0.0f, lhs.b / rhs, 1.0f);
+		return lhs;
+	}
+
+	friend RgbF operator/(const RgbF &lhs, const float rhs)
+	{
+		RgbF temp(lhs);
+		return temp /= rhs;
+	}
+
+	friend RgbF operator-(const RgbF &lhs)
+	{
+		return RgbF(1.0f) - lhs;
+	}
+
+	friend bool operator==(const RgbF &lhs, const RgbF &rhs)
+	{
+		return (FloatingPoint<float>::AlmostEquals(lhs.r, rhs.r)
+				&& FloatingPoint<float>::AlmostEquals(lhs.g, rhs.g)
+				&& FloatingPoint<float>::AlmostEquals(lhs.b, rhs.b));
+	}
+
+	friend bool operator!=(const RgbF &lhs, const RgbF &rhs)
+	{
+		return !(lhs == rhs);
+	}
+
+	float GetIntensity() const
+	{
+		return std::min<float>(0.2126f * r + 0.7152f * g + 0.0722f * b, 1.0f);
+	}
+
+	float r, g, b;
+};
+
+}
+}
