87dc220ebed9980de07778980b5c67da65331a87
Ming Ming <mint2648@gmail.com>
2014-11-03 14:11:21 +0800

    Add ARGB color structs

---
+++ inc/libutils/type/rgba_8.h
@@ -0,0 +1,75 @@
+/*
+ * rgba_8.h
+ *
+ * Author: Ming Tsang
+ * Copyright (c) 2014 Ming Tsang
+ * Refer to LICENSE for details
+ */
+
+#pragma once
+
+#include <cstdint>
+#include <algorithm>
+
+#include "libutils/math/math_utils.h"
+#include "libutils/type/rgb_8.h"
+
+namespace libutils
+{
+namespace type
+{
+
+struct Rgba8
+{
+	Rgba8()
+			: Rgba8(0)
+	{}
+
+	Rgba8(const Rgba8 &rhs) = default;
+
+	explicit Rgba8(const uint8_t val)
+			: Rgba8(val, val, val)
+	{}
+
+	Rgba8(const uint8_t r, const uint8_t g, const uint8_t b)
+			: Rgba8(r, g, b, 0xFF)
+	{}
+
+	Rgba8(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a)
+			: rgb(r, g, b),
+			  a(a)
+	{}
+
+	Rgba8& operator=(const Rgba8 &rhs)
+	{
+		rgb = rhs.rgb;
+		a = rhs.a;
+		return *this;
+	}
+
+	friend bool operator==(const Rgba8 &lhs, const Rgba8 &rhs)
+	{
+		return (lhs.rgb == rhs.rgb && lhs.a == rhs.a);
+	}
+
+	friend bool operator!=(const Rgba8 &lhs, const Rgba8 &rhs)
+	{
+		return !(lhs == rhs);
+	}
+
+	uint32_t ToArgb32() const
+	{
+		return (a << 24) | rgb.ToRgb32();
+	}
+
+	bool IsOpaque() const
+	{
+		return (a == 0xFF);
+	}
+
+	Rgb8 rgb;
+	uint8_t a;
+};
+
+}
+}
---
+++ inc/libutils/type/rgba_f.h
@@ -0,0 +1,72 @@
+/*
+ * rgba_f.h
+ *
+ * Author: Ming Tsang
+ * Copyright (c) 2014 Ming Tsang
+ * Refer to LICENSE for details
+ */
+
+#pragma once
+
+#include <cstdint>
+#include <algorithm>
+
+#include "libutils/math/math_utils.h"
+#include "libutils/type/floating_point.h"
+#include "libutils/type/rgb_f.h"
+
+namespace libutils
+{
+namespace type
+{
+
+struct RgbaF
+{
+	RgbaF()
+			: RgbaF(0)
+	{}
+
+	RgbaF(const RgbaF &rhs) = default;
+
+	explicit RgbaF(const float val)
+			: RgbaF(val, val, val)
+	{}
+
+	RgbaF(const float r, const float g, const float b)
+			: RgbaF(r, g, b, 1.0f)
+	{}
+
+	RgbaF(const float r, const float g, const float b, const float a)
+			: rgb(r, g, b),
+			  a(a)
+	{}
+
+	RgbaF& operator=(const RgbaF &rhs)
+	{
+		rgb = rhs.rgb;
+		a = rhs.a;
+		return *this;
+	}
+
+	friend bool operator==(const RgbaF &lhs, const RgbaF &rhs)
+	{
+		return (lhs.rgb == rhs.rgb
+				&& FloatingPoint<float>::AlmostEquals(lhs.a, rhs.a));
+	}
+
+	friend bool operator!=(const RgbaF &lhs, const RgbaF &rhs)
+	{
+		return !(lhs == rhs);
+	}
+
+	bool IsOpaque() const
+	{
+		return (a >= 1.0f);
+	}
+
+	RgbF rgb;
+	float a;
+};
+
+}
+}
