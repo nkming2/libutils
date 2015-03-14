b8c7797ae4ee7112343d713e1cd0415d40602b91
Ming Ming <mint2648@gmail.com>
2015-03-15 03:25:07 +0800

    Add comment

--- inc/libutils/platform/endian_utils.h
+++ inc/libutils/platform/endian_utils.h
@@ -24,13 +24,44 @@ public:
 		return !IsBigEndian();
 	}
 
+	/**
+	 * Change the endianness of a 2-byte or 4-byte data. Internally it simply
+	 * delegate the call to one of the corresponding Translate* method
+	 *
+	 * @param from
+	 * @return
+	 */
 	template<typename T>
 	static T Translate(const T from);
+	/**
+	 * Change the endianness of a 2-byte data
+	 *
+	 * @param from
+	 * @return
+	 */
 	static uint16_t Translate16(const uint16_t from);
+	/**
+	 * Change the endianness of a 4-byte data
+	 *
+	 * @param from
+	 * @return
+	 */
 	static uint32_t Translate32(const uint32_t from);
 
+	/**
+	 * Covert data from host endian to big endian
+	 *
+	 * @param host
+	 * @return
+	 */
 	template<typename T>
 	static T HostToBe(const T host);
+	/**
+	 * Covert data from host endian to little endian
+	 *
+	 * @param host
+	 * @return
+	 */
 	template<typename T>
 	static T HostToLe(const T host);
 };
